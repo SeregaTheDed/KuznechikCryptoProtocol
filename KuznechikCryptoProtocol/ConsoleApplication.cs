e20e6fae254b1172f48432b5ae6c068b039e9e28
s.shtanko <s.shtanko@krista.ru>
2024-03-20 23:58:11 +0300

    Тест

--- KuznechikCryptoProtocol/ConsoleApplication.cs
+++ KuznechikCryptoProtocol/ConsoleApplication.cs
@@ -9,32 +9,69 @@ namespace KuznechikCryptoProtocol
 {
     internal static class ConsoleApplication
     {
-        private static KuznyechikCryptor kuznyechikDecryptor = new KuznyechikCryptor();
+        private static KuznyechikCryptor kuznyechikCryptor = new KuznyechikCryptor();
+
+        private const string bigFileFilePath = "./Data/BigFile.mp4";
+        private const string dataFilePath = "./Data/data.txt";
+        private const string keyFilePath = "./Data/key.txt";
+
+        private const string encryptedBigFileName = "EncryptedBigFile";
+        private const string decryptedBigFileName = "DecryptedBigFile.mp4";
+        private const string resultPdfFileName = "resultPDF.pdf";
+        
 
         private static void Task1()
         {
             // Вариант 9
-            string keyString = File.ReadAllText("./Data/key.txt");
+            string keyString = File.ReadAllText(keyFilePath);
             byte[] key = Convert.FromHexString(keyString);
 
-            string dataString = File.ReadAllText("./Data/data.txt");
+            string dataString = File.ReadAllText(dataFilePath);
             byte[] data = Convert.FromBase64String(dataString);
 
-            byte[] decriptedData = kuznyechikDecryptor.Decript(data, key).ToArray();
+            byte[] decriptedData = kuznyechikCryptor.Decript(data, key).ToArray();
 
-            using (FileStream fileStream = new FileStream("resultPDF.pdf", FileMode.OpenOrCreate))
+            using (FileStream fileStream = new FileStream(resultPdfFileName, FileMode.OpenOrCreate))
             {
                 fileStream.Write(decriptedData);
             }
-            Console.WriteLine("Расшифрован файл: resultPDF.pdf. Нажмите Enter для продолжения");
+            Console.WriteLine($"Расшифрован файл: {resultPdfFileName}. Нажмите Enter для продолжения...");
             Console.ReadLine();
         }
 
         private static void Task2()
         {
-            double fileSize = new FileInfo("./Data/BigFile.mp4").Length;
-            Console.WriteLine(fileSize / 1024 / 1024);
+            string keyString = File.ReadAllText(keyFilePath);
+            byte[] key = Convert.FromHexString(keyString);
 
+            double fileSize = new FileInfo(bigFileFilePath).Length;
+            double fileSizeMb = Math.Round(fileSize / 1024 / 1024, 2);
+            Console.WriteLine($"Размер файла: { fileSizeMb } Мб");
+
+            Console.WriteLine("Начало обычного шифрования без модификаций. " +
+                "Длительный процесс, придется подождать...");
+            DateTime start1 = DateTime.Now;
+            var encriptedData = kuznyechikCryptor.Encript(File.ReadAllBytes(bigFileFilePath), key);
+            DateTime end1 = DateTime.Now;
+            Console.WriteLine($"Время начала шифрования: {start1}. Время начала шифрования: {end1}. " +
+                $"Общее время: {(end1-start1).TotalSeconds} сек. Скорость: {fileSizeMb / (end1 - start1).TotalSeconds} Мб/сек");
+            using (FileStream fileStream = new FileStream(encryptedBigFileName, FileMode.OpenOrCreate))
+            {
+                fileStream.Write(encriptedData);
+            }
+            Console.WriteLine("Зашифрованный файл: " + encryptedBigFileName);
+
+            var decriptedData = kuznyechikCryptor.Decript(encriptedData, key);
+            using (FileStream fileStream = new FileStream(decryptedBigFileName, FileMode.OpenOrCreate))
+            {
+                fileStream.Write(decriptedData);
+            }
+            Console.WriteLine("Расшифрованный файл: " + decryptedBigFileName);
+
+
+            Console.WriteLine("Нажмите дважды Enter для продолжения...");
+            Console.ReadLine();
+            Console.ReadLine();
         }
 
         public static void Run()
@@ -51,7 +88,13 @@ namespace KuznechikCryptoProtocol
                         case "1": Task1(); break;
                         case "2": Task2(); break;
                         case "3": return;
-                        default: Console.WriteLine("Выберите пункт меню!"); break;
+                        default: 
+                            { 
+                                Console.WriteLine("Выберите пункт меню!");
+                                Console.WriteLine("Нажмите Enter для продолжения");
+                                Console.ReadLine();
+                                break; 
+                            }
                     }
                 } 
                 catch (IOException exception)
@@ -70,7 +113,7 @@ namespace KuznechikCryptoProtocol
             Console.Clear();
             Console.WriteLine("Выберите пункт меню:");
             Console.WriteLine("1. Расшифровать данные (файл Data/data.txt) по ключу (файл Data/key.txt)");
-            Console.WriteLine("2. Зашифровать файл Data/BigFile.mp4 обычным методом и быстрым, вывести статистику");
+            Console.WriteLine("2. Зашифровать файл Data/BigFile.mp4 обычным методом и быстрым по ключу (файл Data/key.txt), вывести статистику");
             Console.WriteLine("3. Выход");
             Console.WriteLine("---------------------------------");
             Console.WriteLine("Все файлы можно подменять на горячую(во время выбора пункта меню).");
