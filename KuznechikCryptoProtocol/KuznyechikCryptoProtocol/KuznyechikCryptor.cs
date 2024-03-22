7593f9feb411d0e4213ec60d44a48535e69f79da
s.shtanko <s.shtanko@krista.ru>
2024-03-22 23:30:01 +0300

    Рефакторинг

--- KuznechikCryptoProtocol/KuznyechikCryptoProtocol/KuznyechikCryptor.cs
+++ KuznechikCryptoProtocol/KuznyechikCryptoProtocol/KuznyechikCryptor.cs
@@ -2,8 +2,8 @@
 {
     internal class KuznyechikCryptor
     {
-        private byte[][] iterC = new byte[32][]; // массив итерационных констант
-        private byte[][] iterK = new byte[10][]; // массив итерационных ключей
+        private byte[][] iterationConstants = new byte[32][]; // массив итерационных констант
+        private byte[][] iterationKeys = new byte[10][]; // массив итерационных ключей
         private readonly Operations operations;
 
         public KuznyechikCryptor(Operations operations)
@@ -11,74 +11,11 @@
             this.operations = operations;
         }
 
-        public byte[] Encript(byte[] file, byte[] masterKey)
+        public byte[] Encript(byte[] originText, byte[] masterKey)
         {
             KeyGen(masterKey);
-            int NumOfBlocks; // Определение кол-ва блоков по 16 байт
-            int NumberOfNull; // Определение кол-ва недостающих байт последнего блока
-            byte[] OriginText = file;
-            byte[] encrText = new byte[file.Length]; // Массив для хранения зашифрованных байтов
-            if ((file.Length % 16) == 0)
-            {
-                NumOfBlocks = file.Length / 16;
-            }
-            else
-            {
-                NumOfBlocks = (file.Length / 16) + 1;
-                NumberOfNull = NumOfBlocks * 16 - file.Length;
-                Array.Resize(ref OriginText, OriginText.Length + NumberOfNull);
-                Array.Resize(ref encrText, OriginText.Length + NumberOfNull);
-                if (NumberOfNull == 1)
-                {
-                    OriginText[OriginText.Length - 1] = 0x80;
-                }
-                else
-                {
-                    for (int i = OriginText.Length - 1; i >= 0; i--)
-                    {
-                        if (i == OriginText.Length - 1)
-                        {
-                            OriginText[OriginText.Length - 1] = 0x81;
-                        }
-                        else if (OriginText[i] != 0)
-                        {
-                            OriginText[i + 1] = 0x01;
-                            break;
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < NumOfBlocks; i++) // Операция зашифровки
-            {
-                byte[] block = new byte[16];
-                for (int j = 0; j < 16; j++)
-                {
-                    block[j] = OriginText[i * 16 + j];
-                }
-
-                for (int j = 0; j < 9; j++)
-                {
-                    block = operations.X(block, iterK[j]);
-                    block = operations.S(block);
-                    block = operations.L(block);
-
-                }
-
-                block = operations.X(block, iterK[9]);
-                for (int j = 0; j < 16; j++)
-                {
-                    encrText[i * 16 + j] = block[j];
-                }
-            }
-            return encrText;
-        }
-
-        public byte[] Decript(byte[] file, byte[] masterKey)
-        {
-            KeyGen(masterKey);
-            int NumOfBlocks = file.Length / 16; // Определение кол-ва блоков по 16 байт
-            byte[] originText = file;
-            byte[] decrText = new byte[file.Length]; // Массив для хранения зашифрованных байтов
+            byte[] encriptedBytes = new byte[originText.Length];
+            AddBytesToGoodSize(originText, out int NumOfBlocks, ref originText, ref encriptedBytes);
             for (int i = 0; i < NumOfBlocks; i++)
             {
                 byte[] block = new byte[16];
@@ -87,36 +24,103 @@
                     block[j] = originText[i * 16 + j];
                 }
 
-                block = operations.X(block, iterK[9]);
+                for (int j = 0; j < 9; j++)
+                {
+                    block = operations.X(block, iterationKeys[j]);
+                    block = operations.S(block);
+                    block = operations.L(block);
+                }
+
+                block = operations.X(block, iterationKeys[9]);
+                for (int j = 0; j < 16; j++)
+                {
+                    encriptedBytes[i * 16 + j] = block[j];
+                }
+            }
+            return encriptedBytes;
+        }
+
+        private static void AddBytesToGoodSize(byte[] file, out int NumOfBlocks, ref byte[] originBytes, ref byte[] encriptedBytes)
+        {
+            int NumberOfNull;
+            if ((file.Length % 16) != 0)
+            {
+                NumOfBlocks = (file.Length / 16) + 1;
+                NumberOfNull = NumOfBlocks * 16 - file.Length;
+                Array.Resize(ref originBytes, originBytes.Length + NumberOfNull);
+                Array.Resize(ref encriptedBytes, originBytes.Length + NumberOfNull);
+                if (NumberOfNull == 1)
+                {
+                    originBytes[originBytes.Length - 1] = 0x80;
+                }
+                else
+                {
+                    for (int i = originBytes.Length - 1; i >= 0; i--)
+                    {
+                        if (i == originBytes.Length - 1)
+                        {
+                            originBytes[originBytes.Length - 1] = 0x81;
+                        }
+                        else if (originBytes[i] != 0)
+                        {
+                            originBytes[i + 1] = 0x01;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                NumOfBlocks = file.Length / 16;
+            }
+        }
+
+        public byte[] Decript(byte[] originBytes, byte[] masterKey)
+        {
+            KeyGen(masterKey);
+            int blockCount = originBytes.Length / 16;
+            byte[] decriptedBytes = new byte[originBytes.Length];
+            for (int i = 0; i < blockCount; i++)
+            {
+                byte[] block = new byte[16];
+                for (int j = 0; j < 16; j++)
+                {
+                    block[j] = originBytes[i * 16 + j];
+                }
+
+                block = operations.X(block, iterationKeys[9]);
 
                 for (int j = 8; j >= 0; j--)
                 {
                     block = operations.LReverse(block);
                     block = operations.SReverse(block);
-                    block = operations.X(block, iterK[j]);
+                    block = operations.X(block, iterationKeys[j]);
 
                 }
 
                 for (int j = 0; j < 16; j++)
                 {
-                    decrText[i * 16 + j] = block[j];
+                    decriptedBytes[i * 16 + j] = block[j];
                 }
             }
-            return decrText;
+            return decriptedBytes;
         }
         private void KeyGen(byte[] mas_key)
         {
             //Генерация раундовых констант
-            byte[][] iterNum = new byte[32][];
+            byte[][] iterationNumber = new byte[32][];
             for (int i = 0; i < 32; i++)
             {
-                iterNum[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, Convert.ToByte(i + 1)];
-                iterC[i] = operations.L(iterNum[i]);
+                iterationNumber[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, Convert.ToByte(i + 1)];
+                iterationConstants[i] = operations.L(iterationNumber[i]);
             }
 
             //Генерация первых 2-х ключей
             byte[] A = new byte[16];
-            for (int i = 0; i < 16; i++) A[i] = mas_key[i];
+            for (int i = 0; i < 16; i++) 
+            { 
+                A[i] = mas_key[i]; 
+            }
             byte[] B = new byte[16];
             int j = 0;
             for (int i = 16; i < 32; i++)
@@ -125,25 +129,21 @@
                 j++;
             }
             j = 0;
-            iterK[0] = A;
-            iterK[1] = B;
+            iterationKeys[0] = A;
+            iterationKeys[1] = B;
 
             byte[] C = new byte[16];
             byte[] D = new byte[16];
 
-            //Генерация остальных ключей
             for (int i = 0; i < 4; i++)
             {
-                operations.F(A, B, ref C, ref D, iterC[0 + 8 * i]);
-                operations.F(C, D, ref A, ref B, iterC[1 + 8 * i]);
-                operations.F(A, B, ref C, ref D, iterC[2 + 8 * i]);
-                operations.F(C, D, ref A, ref B, iterC[3 + 8 * i]);
-                operations.F(A, B, ref C, ref D, iterC[4 + 8 * i]);
-                operations.F(C, D, ref A, ref B, iterC[5 + 8 * i]);
-                operations.F(A, B, ref C, ref D, iterC[6 + 8 * i]);
-                operations.F(C, D, ref A, ref B, iterC[7 + 8 * i]);
-                iterK[2 * i + 2] = A;
-                iterK[2 * i + 3] = B;
+                for (int k = 0; k < 4; k++)
+                {
+                    operations.F(A, B, ref C, ref D, iterationConstants[k*2 + 8 * i]);
+                    operations.F(C, D, ref A, ref B, iterationConstants[k*2 + 1 + 8 * i]);
+                }
+                iterationKeys[2 * i + 2] = A;
+                iterationKeys[2 * i + 3] = B;
             }
         }
 
